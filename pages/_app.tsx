87f76ad263a3c6784dade4567b30021593939e95
Anh Vu <anhvu@Anhs-MacBook-Pro-2.local>
2024-01-09 13:35:33 -0600

    Preload video

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,7 @@ function App({ Component, pageProps }: AppProps) {
         <meta property="og:description" content="Sunday, May 5th, 2024" />
         <link rel="icon" href="/icon1.ico" />
         <link rel="preload" as="font" href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" />
+        <link rel="preload" as="video" href="/binganh.mp4" />
         <script
           className="daum_roughmap_loader_script"
           src="https://ssl.daumcdn.net/dmaps/map_js_init/roughmapLoader.js"
